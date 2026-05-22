f53fbcbe9644cfa6049097910c9d8f4648d75e8a
NI Zhibin <zhibin.ni@cn.bosch.com>
2026-05-22 07:21:28 +0000

    nuscenes v1.0-mini is ok

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 bash ./tools/dist_test.sh \
     projects/configs/sparsedrive_small_stage2.py \
-    ckpt/sparsedrive_stage2.pth \
-    8 \
+    checkpoints/sparsedrive_stage2.pth \
+    1 \
     --deterministic \
-    --eval bbox
-    # --result_file ./work_dirs/sparsedrive_small_stage2/results.pkl
+    --eval bbox \
+    --out ./work_dirs/sparsedrive_small_stage2/results.pkl
